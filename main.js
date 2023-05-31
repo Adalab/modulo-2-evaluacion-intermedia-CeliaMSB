549cd348f0bec662eb887f941cf1a4cefda49c5b
celiamsb <celiam.suarez19@gmail.com>
2023-05-31 10:21:48 +0200

    include const

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log (`hola`)
+`use strict`
+const btn = document.querySelector('.js_btn');
+const play = document.querySelector('.js_play');
+const result = document.querySelector('.js_result');
+const balance = document.querySelector('.js_balance');
+const imput = document.querySelector('.js_imput');
+const select = document.querySelector('.js_select');
